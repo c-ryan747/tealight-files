0e3ebe4ebc578c1bbf54a87ef8cab752d21b48f0
c-ryan747 <c.ryan747@gmail.com>
2014-08-19 11:49:24 +0100

    Running logo/waterwheel.py

--- logo/waterwheel.py
+++ logo/waterwheel.py
@@ -10,13 +10,18 @@ def triangle(side):
   for i in range(0,3):
     move(side)
     turn(120)
+    
+def shape(side,edges):
+  for i in xrange(0,edges):
+    move(side)
+    turn(360/edges)
 
 def waterwheel(edges, size):
   angle = 360 / edges
   decoration = size / 2
   for i in range(0, edges):
     move(size)
-    triangle(decoration)
+    shape(decoration, 360)
     turn(angle)
 
 turn(-90)
