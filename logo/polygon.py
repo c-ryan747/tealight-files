bfb0860805e7c1f8239c921df58071f69e0afd1b
c-ryan747 <c.ryan747@gmail.com>
2014-08-19 11:44:19 +0100

    Running logo/polygon.py

--- logo/polygon.py
+++ logo/polygon.py
@@ -2,10 +2,10 @@ from tealight.logo import move, turn
 
 def polygon(edges, size):
   angle = 360.0 / edges
-  for i in range(0, edges):
+  for i in xrange(0, edges):
     move(size)
     turn(angle)
     
-polygon(6,150) #
+polygon(6,150) #hexagon
 
-polygon(360,150/(360/6))
+polygon(360,150/(360/6)) #crude circle
