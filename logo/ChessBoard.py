4bbbc9459f02c6d8d8b058f634cd2ca78b5ec6e7
c-ryan747 <c.ryan747@gmail.com>
2014-08-19 12:26:02 +0100

    Running logo/ChessBoard.py

--- logo/ChessBoard.py
+++ logo/ChessBoard.py
@@ -1,18 +1,28 @@
 from tealight.logo import (move, turn)
 
 
-def drawSquare(size): #starts + ends top left
+def whiteSquare(size): #starts + ends top left
   for i in xrange(0,4):
     move(size)
     turn(-90)
     
+def blackSquare(size): #starts + ends top left
+  for i in xrange(0,4):
+    move(size)
+    turn(-90)  
+    
 def chessBoard(gridSize, boxSize):
   for j in xrange(0,gridSize):
-    
-    
     for i in xrange(0,gridSize):
-      drawSquare(boxSize)
+      
+      if ((j*gridSize+i)%2 == 0):
+        whiteSquare(boxSize)
+      else:
+        blackSquare(boxSize) 
+      
       move(boxSize)
+      
+    #correct position
     turn(-90)
     move(boxSize)
     turn(-90)
@@ -20,5 +30,4 @@ def chessBoard(gridSize, boxSize):
     turn(180)
 
 turn(180)  
- #face down
 chessBoard(5,50)
