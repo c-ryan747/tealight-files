9ed095769c24e1bc65c3661ec1617089aa9e02f3
c-ryan747 <c.ryan747@gmail.com>
2014-08-19 11:43:20 +0100

    Running logo/spiral.py

--- logo/spiral.py
+++ logo/spiral.py
@@ -2,11 +2,11 @@ from tealight.logo import move, turn
 
 def spiral(size):
   
-  if size > 300:
+  if size > 300: #terminate after 300 rounds
     return
   
   move(size)
-  turn(60)
-  spiral(size + 5)
+  turn(60) #interior angle
+  spiral(size + 5) #make new side slightly bigger
   
 spiral(0)
