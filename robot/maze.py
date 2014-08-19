c9949f05c779ab292b3889e243767438caf301a7
c-ryan747 <c.ryan747@gmail.com>
2014-08-19 14:57:15 +0100

    Running robot/maze.py

--- robot/maze.py
+++ robot/maze.py
@@ -13,7 +13,7 @@ while True:
   move()
   if (touch() != "wall"):
     a = 1
-  elif (touch() == "wall" and left_side() != "wall"):
+  elif (left_side() != "wall"):
     turn(-1)
   elif (touch() == "wall" and right_side() != "wall"):
     turn(1)
