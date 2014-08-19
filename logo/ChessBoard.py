e93e05f08fcac4507fd680e6533f40b4753d7a0e
c-ryan747 <c.ryan747@gmail.com>
2014-08-19 11:59:17 +0100

    Running logo/ChessBoard.py

--- logo/ChessBoard.py
+++ logo/ChessBoard.py
@@ -7,8 +7,8 @@ def drawSquare(size):
     turn(90)
     
 def chessBoard(gridSize, boxSize):
-  for i in xrange(0,1):
-    for j in xrange(0,gridSize):
+  for i in xrange(1,gridSize):
+    for j in xrange(1,gridSize):
       drawSquare(boxSize)
       move(boxSize)
      
