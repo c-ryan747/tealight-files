71a5b9a72a857eb18868d240f1752a7f6be5bec3
c-ryan747 <c.ryan747@gmail.com>
2014-08-19 14:54:01 +0100

    Running robot/maze.py

--- robot/maze.py
+++ robot/maze.py
@@ -7,12 +7,11 @@ from tealight.robot import (move,
                             right_side)
 
 # Add your code here
+
+    
 while True:
   move()
   if (touch() != "wall"):
     a = 1
-  elif (left_side() != "wall"):
-    turn(-1)
-
-  elif (touch() == "wall"):
-    turn(2)
+  elif (touch() == "wall" and right_side() != "wall"):
+    turn(1)
