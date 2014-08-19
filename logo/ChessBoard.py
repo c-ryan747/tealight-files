2bdf4f0a20d874c2fedb4b9a5d356b9f8b095ce1
c-ryan747 <c.ryan747@gmail.com>
2014-08-19 11:57:37 +0100

    Closing logo/ChessBoard.py

--- logo/ChessBoard.py
+++ logo/ChessBoard.py
@@ -1,1 +1,8 @@
-print "A new file!"
+from tealight.logo import (move, turn)
+
+def drawSquare(size):
+
+def chessBoard(gridSize, boxSize):
+  for i in xrange(0,gridSize):
+    for j in xrange(0,gridSize):
+      
