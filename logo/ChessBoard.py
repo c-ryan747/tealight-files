7b19558baae8fa99724f1563c8d5d680e4ceee96
c-ryan747 <c.ryan747@gmail.com>
2014-08-19 11:59:00 +0100

    Running logo/ChessBoard.py

--- logo/ChessBoard.py
+++ logo/ChessBoard.py
@@ -1,8 +1,15 @@
 from tealight.logo import (move, turn)
 
-def drawSquare(size):
 
+def drawSquare(size):
+  for i in xrange(0,4):
+    move(side)
+    turn(90)
+    
 def chessBoard(gridSize, boxSize):
-  for i in xrange(0,gridSize):
+  for i in xrange(0,1):
     for j in xrange(0,gridSize):
+      drawSquare(boxSize)
+      move(boxSize)
+     
       
