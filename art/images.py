855a17012fe7e0af935dd93a7071cd115cac903a
c-ryan747 <c.ryan747@gmail.com>
2014-08-20 09:11:48 +0100

    Running art/images.py

--- art/images.py
+++ art/images.py
@@ -6,10 +6,20 @@ y = 150
 width = 20
 height = 8
 
-for i in range(0,width):
-  for j in range(0,height):
-    if i % 4 == 0:
-      image(x + i * 60, y + j * 60, "misc/YellowFlower.png")
-    else:
-      image(x + i * 60, y + j * 60, "misc/Clover.png")
+def vertical():
+  for i in range(0,width):
+    for j in range(0,height):
+      if i % 4 == 0:
+        image(x + i * 60, y + j * 60, "misc/YellowFlower.png")
+      else:
+        image(x + i * 60, y + j * 60, "misc/Clover.png")
      
+def horizontal():
+  for i in range(0,width):
+    for j in range(0,height):
+      if j % 4 == 0:
+        image(x + i * 60, y + j * 60, "misc/YellowFlower.png")
+      else:
+        image(x + i * 60, y + j * 60, "misc/Clover.png")
+        
+horizontal()
