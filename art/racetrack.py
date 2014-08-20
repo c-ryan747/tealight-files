6510c1848353a69ed2c87c717a2c233eebc0e2f9
c-ryan747 <c.ryan747@gmail.com>
2014-08-20 12:23:54 +0100

    Running art/racetrack.py

--- art/racetrack.py
+++ art/racetrack.py
@@ -3,7 +3,7 @@ from tealight.art import (color, image, line, spot, circle, box, image, text, ba
 #background ("track.png")
 
 def draw_car(x,y,angle, col):
-  colour(col)
+  color(col)
   line(x,y,100*sin(angle),100*sin(angle))
   
 draw_car(200,200,0,"black")
