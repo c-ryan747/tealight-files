58e0bdff0c38cd7c4047731a2d030b22c74b2487
c-ryan747 <c.ryan747@gmail.com>
2014-08-21 15:18:46 +0100

    Running art/test.py

--- art/test.py
+++ art/test.py
@@ -1,11 +1,4 @@
-  def circle_points(amount, radius,cx,cy,corner,array):
-    angle = 0
-    for i in xrange(1,amount):
-      x = sin(angle)*radius
-      y = cos(angle)*radius
-      if corner == 1:
-        array.append((x+cx,y+cy))
-      angle = angle + (pi/2)/i
+
       
 angle = 0 
 for i in xrange(1,amount):
