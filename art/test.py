9dabda42f3bda71dd4568ba1fee333c891dc93ad
c-ryan747 <c.ryan747@gmail.com>
2014-08-21 11:30:19 +0100

    Running art/test.py

--- art/test.py
+++ art/test.py
@@ -1,8 +1,13 @@
 from tealight.art import polygon,fill_polygon,test_polygon, screen_width, screen_height
 
-points = [(screen_width/4,screen_height/4),
+middle = [(screen_width/4,screen_height/4),
           ((3*screen_width/4),screen_height/4),
           ((3*screen_width/4),(3*screen_height/4)),
           (screen_width/4,(3*screen_height/4))]
+top = [(0,0),
+       (0,screen_height/10),
+       (screen_width,screen_height/10),
+       (screen_width,0)]
+
 
 fill_polygon(points)
