90557624f3566bb933bd1b81a2836567550f6473
c-ryan747 <c.ryan747@gmail.com>
2014-08-21 10:52:22 +0100

    Running art/carClass.py

--- art/carClass.py
+++ art/carClass.py
@@ -1,1 +1,38 @@
-print "A new file!"
+from tealight.art import (color, image, line, spot, circle, box, image, text, background)
+from tealight.utils import sleep
+from math import sin, cos, pi
+
+#background ("track.png")
+
+  
+class car:
+  "Race car class"
+  def __init__(self,x,y,angle):
+    self.draw(x,y,angle,"black")
+    
+  def draw(self,x,y,angle,colour):
+    angle = -angle
+    
+    self.points = [x-100*sin(angle),
+                   y-100*cos(angle),
+                   x-100*sin(angle+(2*pi/3)),
+                   y-100*cos(angle+(2*pi/3)),
+                   x-100*sin(angle+(4*pi/3)),
+                   y-100*cos(angle+(4*pi/3))]
+    
+    self.draw_points(self.points,colour)
+  
+  def draw_points(self,points,colour):
+    color(colour)
+    line(points[0],points[1],points[2],points[3])
+    line(points[2],points[3],points[4],points[5])
+    line(points[4],points[5],points[0],points[1])
+  
+  def move(self,x,y,angle):
+    self.draw_points(self.points,"white")
+    self.draw(x,y,angle,"black")
+    
+  
+
+#player1 = car(200,200,0)
+#player1.move(200,200,pi/2)
