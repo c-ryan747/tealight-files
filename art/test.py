f845c488fb7c148104c5b5ec0e97941497a1f2cd
c-ryan747 <c.ryan747@gmail.com>
2014-08-21 11:24:54 +0100

    Running art/test.py

--- art/test.py
+++ art/test.py
@@ -1,4 +1,4 @@
-from tealight.art import polygon, screen_width, screen_height
+from tealight.art import polygon,fill_polygon, screen_width, screen_height
 
 polygon([(screen_width/4,screen_width/4),(1,10),(10,10),(10,1)])
 fill_polygon([])
