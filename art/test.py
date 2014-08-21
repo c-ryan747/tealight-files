4534941d370de56a0322799952f35174d67002f9
c-ryan747 <c.ryan747@gmail.com>
2014-08-21 12:03:28 +0100

    Running art/test.py

--- art/test.py
+++ art/test.py
@@ -2,34 +2,46 @@ from tealight.art import polygon,fill_polygon,test_polygon, screen_width, screen
 
 class map:
   "Some map disc"
-  def __init--
-middle = [(screen_width/4,screen_height/4),
-          ((3*screen_width/4),screen_height/4),
-          ((3*screen_width/4),(3*screen_height/4)),
-          (screen_width/4,(3*screen_height/4))]
-top = [(0,0),
-       (0,screen_height/10),
-       (screen_width,screen_height/10),
-       (screen_width,0)]
-
-bottom = [(0,screen_height),
-       (screen_width,screen_height),
-       (screen_width,(9*screen_height)/10),
-       (0,(9*screen_height)/10)]
-
-left = [(screen_width,0),
-       (screen_width,screen_height),
-       ((9*screen_width/10),screen_height),
-       ((9*screen_width/10),0)]
-
-right = [(0,0),
-       (0,screen_height),
-       (screen_width/10,screen_height),
-       (screen_width/10,0)]
-
-
-fill_polygon(middle)
-fill_polygon(top)
-fill_polygon(bottom)
-fill_polygon(right)
-fill_polygon(left)
+  def __init__(self):
+    self.polygons = []
+    self.create_polygons()
+    self.draw_polygons()
+    
+  def create_polygons:
+    middle = [(screen_width/4,screen_height/4),
+              ((3*screen_width/4),screen_height/4),
+              ((3*screen_width/4),(3*screen_height/4)),
+              (screen_width/4,(3*screen_height/4))]
+    self.polygons.append(middle)
+    
+    top = [(0,0),
+           (0,screen_height/10),
+           (screen_width,screen_height/10),
+           (screen_width,0)]
+    self.polygons.append(top)
+    
+    bottom = [(0,screen_height),
+              (screen_width,screen_height),
+              (screen_width,(9*screen_height)/10),
+              (0,(9*screen_height)/10)]
+    self.polygons.append(bottom)
+    
+    left = [(screen_width,0),
+            (screen_width,screen_height),
+            ((9*screen_width/10),screen_height),
+            ((9*screen_width/10),0)]
+    self.polygons.append(left)
+    
+    right = [(0,0),
+             (0,screen_height),
+             (screen_width/10,screen_height),
+             (screen_width/10,0)]
+    self.polygons.append(right)
+    
+  def draw_polygons:
+    color("blue")
+    for i in self.polygons:
+      fill_polygon(i)
+    
+    
+mainMap = map()
