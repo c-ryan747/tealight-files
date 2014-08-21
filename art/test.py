7c42424b21ea1e88a01391141ad430a3c1784ac2
c-ryan747 <c.ryan747@gmail.com>
2014-08-21 15:22:11 +0100

    Running art/test.py

--- art/test.py
+++ art/test.py
@@ -3,12 +3,14 @@ from math import pi,sin,cos
 amount = 5
 angle = 0
 radius = 100
+cx = 1000
+cy = 1000
 
 print(pi/2)
 for i in xrange(1,amount+2):
 #  print(angle)
   
-  x = cos(angle)*radius
-  y = sin(angle)*radius
+  x = cos(angle)*radius +cx
+  y = sin(angle)*radius +cy
   print("x:",str(x),"y:",str(y))
   angle = angle + (pi/2)/amount
