45e3d9924f99d0a5e247c35a9355ad0c2722d688
c-ryan747 <c.ryan747@gmail.com>
2014-08-21 11:55:38 +0100

    Running art/test.py

--- art/test.py
+++ art/test.py
@@ -16,8 +16,8 @@ bottom = [(0,screen_height),
 
 left = [(screen_width,0),
        (screen_width,screen_height),
-       (screen_width/10,screen_height),
-       (screen_width/10,0)]
+       ((9*screen_width/10),screen_height),
+       ((9*screen_width/10),0)]
 
 right = [(0,0),
        (0,screen_height),
