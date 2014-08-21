0847344410539a765ecdf1117dfa625ec16068e1
c-ryan747 <c.ryan747@gmail.com>
2014-08-21 10:44:52 +0100

    Running art/racetrack.py

--- art/racetrack.py
+++ art/racetrack.py
@@ -6,23 +6,34 @@ from math import sin, cos, pi
   
 class car:
   "Race car class"
-  def __init__(self):
-    return
-  def draw(self,x,y,angle):
+  def __init__(self,x,y,angle):
+    self.draw(x,y,angle,colour)
+    
+  def draw(self,x,y,angle,colour):
     angle = -angle
+    
     self.points = [x-100*sin(angle),
                    y-100*cos(angle),
                    x-100*sin(angle+(2*pi/3)),
                    y-100*cos(angle+(2*pi/3)),
                    x-100*sin(angle+(4*pi/3)),
                    y-100*cos(angle+(4*pi/3))]
+    self.x = x
+    self.y = y
+    self.angle = angle
+    
+    color(colour)
     
     line(self.points[0],self.points[1],self.points[2],self.points[3])
     line(self.points[2],self.points[3],self.points[4],self.points[5])
     line(self.points[4],self.points[5],self.points[0],self.points[1])
-
+  
+  def move(self,x,y,angle):
+    self.draw(self.x,self.y,self.angle,"white")
+    self.draw(x,y,angle,"black")
+    
   
 
-player1 = car()
-player1.draw(200,200,0)
-player1.draw(200,200,pi/2)
+player1 = car(200,200,0)
+sleep(2000)
+player1.move(200,200,1)
