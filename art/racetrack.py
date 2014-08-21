babef9aa2397393309e05ea8651c9bb750d5e5aa
c-ryan747 <c.ryan747@gmail.com>
2014-08-21 10:29:26 +0100

    Running art/racetrack.py

--- art/racetrack.py
+++ art/racetrack.py
@@ -6,9 +6,19 @@ from math import sin, cos, pi
 def draw_car(x,y,angle, col):
   color(col)
   line(x-100*sin(angle),y-100*cos(angle),x,y)
-  line(x-100*sin(angle+(2*pi/3)),y-100*cos(angle+(2*pi/3)),x,y)
-  line(x-100*sin(angle+(4*pi/3)),y-100*cos(angle+(4*pi/3)),x,y)
+  line(,x,y)
+  line(,x,y)
   
+  
+class car:
+  "Race car class"
+  def __init__(self, x, y, angle):
+    draw(x,y,angle)
+  def draw(x,y,angle):
+    color(col)
+    line(x-100*sin(angle),y-100*cos(angle),x-100*sin(angle+(2*pi/3)),y-100*cos(angle+(2*pi/3)))
+    line(x-100*sin(angle+(2*pi/3)),y-100*cos(angle+(2*pi/3)),x-100*sin(angle+(4*pi/3)),y-100*cos(angle+(4*pi/3)))
+    line(x-100*sin(angle+(4*pi/3)),y-100*cos(angle+(4*pi/3)),x-100*sin(angle),y-100*cos(angle))
   
   
   
