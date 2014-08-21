4a31648056f149ac12f9d13201538facb6846a20
c-ryan747 <c.ryan747@gmail.com>
2014-08-21 11:27:59 +0100

    Running art/test.py

--- art/test.py
+++ art/test.py
@@ -1,8 +1,8 @@
 from tealight.art import polygon,fill_polygon,test_polygon, screen_width, screen_height
 
-points = [(screen_width/4,screen_width/4),
-          ((3*screen_width/4),screen_width/4),
-          ((3*screen_width/4),(3*screen_width/4)),
-          (screen_width/4,(3*screen_width/4))]
+points = [(screen_width/4,screen_height/4),
+          ((3*screen_width/4),screen_height/4),
+          ((3*screen_width/4),(3*screen_height/4)),
+          (screen_width/4,(3*screen_height/4))]
 
 fill_polygon(points)
