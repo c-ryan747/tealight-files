cfb4413b88f319e4f9dd3aca42f9fd60bd5bef01
c-ryan747 <c.ryan747@gmail.com>
2014-08-21 11:31:43 +0100

    Running art/test.py

--- art/test.py
+++ art/test.py
@@ -9,6 +9,12 @@ top = [(0,0),
        (screen_width,screen_height/10),
        (screen_width,0)]
 
+bottom = [(0,screen_height),
+       (screen_width,screen_height),
+       (screen_width,(9*screen_height)/10),
+       (0,(9*screen_height)/10)]
+
 
 fill_polygon(middle)
 fill_polygon(top)
+fill_polygon(bottom)
