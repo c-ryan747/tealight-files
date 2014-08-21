c7dbf290d4545d67e2172f352e0ba7f00449c5cd
c-ryan747 <c.ryan747@gmail.com>
2014-08-21 10:25:27 +0100

    Running art/racetrack.py

--- art/racetrack.py
+++ art/racetrack.py
@@ -7,6 +7,7 @@ def draw_car(x,y,angle, col):
   color(col)
   line(x-100*sin(angle),y-100*cos(angle),x,y)
   line(x-100*sin(angle+(2*pi/3)),y-100*cos(angle+(2*pi/3)),x,y)
+  line(x-100*sin(angle+(4*pi/3)),y-100*cos(angle+(4*pi/3)),x,y)
   
   
   
