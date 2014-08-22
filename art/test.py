55d13997a3e516e0a15d5ca8f51ea6ace76daa5d
c-ryan747 <c.ryan747@gmail.com>
2014-08-22 08:57:08 +0100

    Running art/test.py

--- art/test.py
+++ art/test.py
@@ -93,8 +93,8 @@ class track:
     amount = amount*4
     array = []
     for i in xrange(0,amount):
-      x = x + cos(angle)*radius - radius
-      y = y + sin(angle)*radius - radius
+      x = x + radius - cos(angle)*radius
+      y = y + radius - sin(angle)*radius
 
       if i>= (3*amount)/4:
         y = y + width
