d07ab142206caaaf2040cb8e0aa159edd66b9dfd
c-ryan747 <c.ryan747@gmail.com>
2014-08-22 09:02:26 +0100

    Running art/carClass.py

--- art/carClass.py
+++ art/carClass.py
@@ -12,7 +12,7 @@ class car:
     self.draw(x,y,angle,"black")
     
   def draw(self,x,y,angle,colour):
-    size = screen_width/100
+    size = screen_width/70
 
     self.points = [x+size*cos(angle),
                    y-size*sin(angle),
