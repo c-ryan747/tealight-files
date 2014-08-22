b49cdb94a8ed2d89322aacca916afdc56f2ddf7f
c-ryan747 <c.ryan747@gmail.com>
2014-08-22 09:29:39 +0100

    Running art/mapClass.py

--- art/mapClass.py
+++ art/mapClass.py
@@ -2,7 +2,7 @@ from tealight.art import color,box, polygon,fill_polygon,test_polygon, screen_wi
 from math import pi,sin,cos
 
 class track:
-  "Some map disc"
+  "Some map description"
   def __init__(self):
     self.polygons = []
     self.top = []
@@ -22,20 +22,12 @@ class track:
     middle = self.circle_points_b(sides, screen_width/8,(5*screen_width)/8,(3*screen_height)/8,1,middle)
     middle = self.circle_points_c(sides, screen_width/8,(5*screen_width)/8,(5*screen_height)/8,1,middle)
     middle = self.circle_points_d(sides, screen_width/8,(3*screen_width)/8,(5*screen_height)/8,1,middle)
-    
-    #middle.append(((3*screen_width)/4,screen_height/4))
-    #middle.append(((3*screen_width)/4,(3*screen_height)/4))
-    #middle.append((screen_width/4,(3*screen_height)/4))
-    #part = [(,
-    #          ((3*screen_width/4),(3*screen_height/4)),
-    #          (screen_width/4,(3*screen_height/4))]
-    #middle.append(part)
     self.polygons.append(middle)
     
-    self.top = [(0,0),
-           (0,screen_height/10),
-           (screen_width,screen_height/10),
-           (screen_width,0)]
+    self.top = [(0,0),                          #static so not drawn, for collision only
+                (0,screen_height/10),
+                (screen_width,screen_height/10),
+                (screen_width,0)]
     
     bottom = [(0,screen_height),
               (screen_width,screen_height),
@@ -50,46 +42,53 @@ class track:
     self.polygons.append(right)
     
     left = [(0,(screen_height/10)-3),
-             (0,screen_height),
-             (screen_width/10,screen_height),
-             (screen_width/10,(screen_height/10)-3)]
+            (0,screen_height),
+            (screen_width/10,screen_height),
+            (screen_width/10,(screen_height/10)-3)]
     self.polygons.append(left)
     
     self.top_detector = [((screen_width*0.45),screen_height/10),
                          ((screen_width*0.45),screen_height/3),
                          ((screen_width*0.55),screen_height/3),
                          ((screen_width*0.55),screen_height/10)]
+    
     self.bottom_detector = [((screen_width*0.45),screen_height*0.9),
                             ((screen_width*0.45),screen_height*0.66),
                             ((screen_width*0.55),screen_height*0.66),
                             ((screen_width*0.55),screen_height*0.9)]
     
   def draw_polygons(self):
+    #detectors
     color("red")
     polygon(self.top_detector)
     polygon(self.bottom_detector)
+    
+    #map
     color("blue")
     for i in self.polygons:
-      print(i)
       fill_polygon(i)
     
 
-      
   def test_point(self,x,y):
+    #invisible one
     if test_polygon(x,y,self.top):
       return False
+    #rest
     for i in self.polygons:
       if test_polygon(x,y,i):
         return False
     return True
   
+  
   def test_in_top_detector(self,x,y):
     return test_polygon(x,y,self.top_detector)
+  
   
   def test_in_bottom_detector(self,x,y):
     return test_polygon(x,y,self.bottom_detector)
   
-  def circle_points_a(self,amount, radius,cx,cy,corner,array):
+  
+  def circle_points_a(self,amount, radius,cx,cy,corner,array): #top left curve
     angle = 0
     for i in xrange(1,amount+2):
       x = cx- (cos(angle)*radius) 
@@ -98,7 +97,9 @@ class track:
       #print("x:",str(x),"y:",str(y))
       angle = angle + (pi/2)/amount
     return array
-  def circle_points_b(self,amount, radius,cx,cy,corner,array):
+  
+  
+  def circle_points_b(self,amount, radius,cx,cy,corner,array): #top right curve
     angle = 0
     for i in xrange(1,amount+2):
       x = cx+(sin(angle)*radius) 
@@ -107,7 +108,9 @@ class track:
       #print("x:",str(x),"y:",str(y))
       angle = angle + (pi/2)/amount
     return array 
-  def circle_points_c(self,amount, radius,cx,cy,corner,array):
+  
+  
+  def circle_points_c(self,amount, radius,cx,cy,corner,array):  #bottom right curve
     angle = 0
     for i in xrange(1,amount+2):
       x = cx+(cos(angle)*radius) 
@@ -116,7 +119,9 @@ class track:
       #print("x:",str(x),"y:",str(y))
       angle = angle + (pi/2)/amount
     return array
-  def circle_points_d(self,amount, radius,cx,cy,corner,array):
+  
+  
+  def circle_points_d(self,amount, radius,cx,cy,corner,array): #bottom left curve
     angle = 0
     for i in xrange(1,amount+2):
       x = cx-(sin(angle)*radius) 
