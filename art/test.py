4a286a117d2847b4fc9d90c2a4245623bd8d9e5d
c-ryan747 <c.ryan747@gmail.com>
2014-08-22 09:08:45 +0100

    Running art/test.py

--- art/test.py
+++ art/test.py
@@ -19,6 +19,7 @@ class track:
     
     middle = self.circle_points_a(5, screen_width/8,(3*screen_width)/8,(3*screen_height)/8,1,middle)
     middle = self.circle_points_b(5, screen_width/8,(5*screen_width)/8,(3*screen_height)/8,1,middle)
+    middle = self.circle_points_b(5, screen_width/8,(5*screen_width)/8,(5*screen_height)/8,1,middle)
     
     #middle.append(((3*screen_width)/4,screen_height/4))
     middle.append(((3*screen_width)/4,(3*screen_height)/4))
@@ -100,6 +101,15 @@ class track:
     for i in xrange(1,amount+2):
       x = cx+(sin(angle)*radius) 
       y = cy-(cos(angle)*radius)
+      array.append((x,y))
+      #print("x:",str(x),"y:",str(y))
+      angle = angle + (pi/2)/amount
+    return array 
+  def circle_points_b(self,amount, radius,cx,cy,corner,array):
+    angle = 0
+    for i in xrange(1,amount+2):
+      x = cx+(sin(angle)*radius) 
+      y = cy+(cos(angle)*radius)
       array.append((x,y))
       #print("x:",str(x),"y:",str(y))
       angle = angle + (pi/2)/amount
