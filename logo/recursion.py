2e0827ded9455980b1f571afe268d68fff3b80de
c-ryan747 <c.ryan747@gmail.com>
2014-08-24 18:00:46 +0100

    Running logo/recursion.py

--- logo/recursion.py
+++ logo/recursion.py
@@ -2,4 +2,29 @@ from tealight.logo import move, turn
 
 def zig(n):
   if n == 1:
-    turn(
+    turn(-90)
+    move(10)
+    turn(-90)
+    move(10)
+  else:
+    zig(n/2)
+    zag(n/2)
+    zig(n/2)
+    zag(n/2)
+    
+def zag(n):
+  if n == 1:
+    turn(90)
+    move(10)
+    turn(90)
+    move(10)
+    turn(-90)
+    move(10)
+  else:
+    zag(n/2)
+    zag(n/2)
+    zig(n/2)
+    zag(n/2)
+    
+zig(8)
+zig(8)
