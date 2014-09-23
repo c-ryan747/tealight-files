4dc68b0d3a109cf667815bebf9f04214d5f0c500
c-ryan747 <c.ryan747@gmail.com>
2014-09-23 13:01:56 +0100

    Running art/newOrbit.py

--- art/newOrbit.py
+++ art/newOrbit.py
@@ -1,1 +1,88 @@
-print "A new file!"
+from tealight.art import (color, line, spot, circle, box, image, text, background)
+import math
+
+x = 300
+y = 200
+vx = 0
+vy = 0
+ax = 0
+ay = 0
+
+power = 0.4
+friction = 0.1
+gravity = 0.2
+
+def handle_keydown(key):
+  global ax, ay
+  
+  if key == "left":
+    ax = -power
+  elif key == "right":
+    ax = power
+  elif key == "up":
+    ay = -power
+  elif key == "down":
+    ay = power
+
+def handle_keyup(key):
+  global ax, ay
+
+  if key == "left" or key == "right":
+    ax = 0
+  elif key == "up" or key == "down":
+    ay = 0
+    
+def apply_friction(v):
+  if v>0:
+    if v>friction:
+      v = v - friction
+    else:
+      v = 0
+  elif v<0:
+    if v<-friction:
+      v = v + friction
+    else:
+      v = 0
+  return v
+
+
+def apply_gravity():
+  global x,y,vx,vy
+  deltax = 300-x
+  deltay = 200-y
+  
+  vx = vx + deltax/50
+  vy = vy + deltay/50
+  
+  
+def handle_frame(): 
+  global x,y,vx,vy,ax,ay
+  
+  color("white")
+  
+  spot(x,y,8)
+  
+    
+  vx = vx + ax
+  vy = vy + ay
+  
+  
+  #gravity
+  vy = vy + gravity
+  apply_gravity()
+  
+  #friction
+  vx = apply_friction(vx)
+  vy = apply_friction(vy)
+  
+
+  
+  
+  x = x + vx
+  y = y + vy
+  
+  color("blue")
+  
+  spot(x,y,8)
+  
+  
