ebba53e36b623f27566f474e73ed29960696cf87
c-ryan747 <c.ryan747@gmail.com>
2014-10-22 19:22:37 +0100

    Running art/bezier.py

--- art/bezier.py
+++ art/bezier.py
@@ -1,6 +1,6 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 
-pointA = (500,500)
-pointB = (700,300)
+pointA = (300,500)
+pointB = (200,300)
 
 line(pointA[0],pointA[1],pointB[0],pointB[1])
