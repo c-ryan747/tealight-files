bbeda5652f7d51caecf8cd3d6eb8642705b70106
c-ryan747 <c.ryan747@gmail.com>
2014-10-22 19:31:15 +0100

    Running art/bezier.py

--- art/bezier.py
+++ art/bezier.py
@@ -11,3 +11,10 @@ line(pointA[0],pointA[1],pointB[0],pointB[1])
 
 line(pointA[0],pointA[1],controlA[0],controlA[1])
 line(pointB[0],pointB[1],controlB[0],controlB[1])
+
+colour("black")
+
+for i in xrange(1,100):
+  pointAC = (pointA[0] + (i*(controlA[0]-pointA[0])/100,pointA[1] + (i*(controlA[1]-pointA[1])/100)
+  pointBC = (controlB[0] + (i*(pointB[0]-controlB[0])/100,controlB[1] + (i*(pointB[1]-controlB[1])/100)
+  
