0a539a1f31568b4c8eddefe2504ea213fef84229
c-ryan747 <c.ryan747@gmail.com>
2014-10-22 19:37:47 +0100

    Running art/bezier.py

--- art/bezier.py
+++ art/bezier.py
@@ -1,5 +1,8 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 
+def partWayBetween(a,b,i):
+  return (a[0] + (i*(b[0]-a[0])/100),a[1] + (i*(b[1]-a[1])/100))
+
 pointA = (300,500)
 pointB = (500,300)
 
@@ -15,9 +18,11 @@ line(pointB[0],pointB[1],controlB[0],controlB[1])
 color("black")
 
 for i in xrange(1,50):
-  pointAC = (pointA[0] + (i*(controlA[0]-pointA[0])/100),pointA[1] + (i*(controlA[1]-pointA[1])/100))
+  pointAC = partWayBetween(pointA, controlA, i)
   pointBC = (controlB[0] + (i*(pointB[0]-controlB[0])/100),controlB[1] + (i*(pointB[1]-controlB[1])/100))
   pointCC = (controlA[0] + (i*(controlB[0]-controlA[0])/100),controlA[1] + (i*(controlB[1]-controlA[1])/100))
   spot(pointAC[0],pointAC[1],5)
   spot(pointBC[0],pointBC[1],5)
   spot(pointCC[0],pointCC[1],5)
+  
+  pointACCC = pointAC = (pointA[0] + (i*(controlA[0]-pointA[0])/100),pointA[1] + (i*(controlA[1]-pointA[1])/100))
