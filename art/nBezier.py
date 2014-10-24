5a2779d62973fa00ca7357eaf32bddc42883bd14
c-ryan747 <c.ryan747@gmail.com>
2014-10-24 14:24:47 +0100

    Running art/nBezier.py

--- art/nBezier.py
+++ art/nBezier.py
@@ -22,6 +22,6 @@ def drawBezier(points,n):
     line(lastDrawn[0],lastDrawn[1],tempPoints[0][0],tempPoints[0][1])
     lastDrawn = tempPoints[0]
   
-drawBezier([(50,300),(50,500),(300,100),(300,300)],1000.0)
+drawBezier([(50,300),(50,500),(200,100),(200,300)],1000.0)
 #drawBezier([(300,300),(350,200),(500,300),(450,150)],1000.0)
 #drawBezier([(550,250),(550,100),(750,250),(750,400)],1000.0)
