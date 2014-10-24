644b9b6aac1281e5c85f0f4d4afd9bb6b10f6a17
c-ryan747 <c.ryan747@gmail.com>
2014-10-24 14:24:22 +0100

    Running art/nBezier.py

--- art/nBezier.py
+++ art/nBezier.py
@@ -16,8 +16,9 @@ def drawBezier(points,n):
       for j in range(0,len(tempPoints)-1):
         print "j: " +str(j) +" tempPoints" + str(tempPoints)
         tempPoints[j] = partWayBetween(tempPoints[j],tempPoints[j+1],k)
+      
       tempPoints.pop()
-    print tempPoints
+   
     line(lastDrawn[0],lastDrawn[1],tempPoints[0][0],tempPoints[0][1])
     lastDrawn = tempPoints[0]
   
