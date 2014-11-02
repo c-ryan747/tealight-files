99ca8538e098e63725a9105544e550578a83b990
c-ryan747 <c.ryan747@gmail.com>
2014-11-02 21:28:36 +0000

    Running art/RSA.py

--- art/RSA.py
+++ art/RSA.py
@@ -1,3 +1,6 @@
+from random import randint
+import math
+
 # Two random 
 P = 53
 q = 59
@@ -7,4 +10,16 @@ phin = (q-1)(p-1)
 
 foundE = False
 while !foundE:
-  
+  e = randint(2,phin-1)
+  if n%e != 0:
+    foundE = True
+
+d = (2*phin + 1)/e    
+    
+data = 89
+c = math.pow(data,e)%n
+
+decrypted = math.pow(c,d)%n
+
+if decrypted == data:
+  print "Success"
