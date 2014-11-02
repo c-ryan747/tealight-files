6e0408b612da4f63419c8484b43c93cb79f390d9
c-ryan747 <c.ryan747@gmail.com>
2014-11-02 19:30:28 +0000

    Running art/imageCompression.py

--- art/imageCompression.py
+++ art/imageCompression.py
@@ -6,7 +6,6 @@ box(0,0,10000,10000)
 
 def randomGridData(n):
   data = [[0 for x in xrange(n)] for x in xrange(n)] 
-  print data
   for i in xrange(0,n):
     for j in xrange(0,n):
       data[i][j] = randint(0,1)
