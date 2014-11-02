9ff194ce526e3fd2df148ae26dcb3bed5b2266fc
c-ryan747 <c.ryan747@gmail.com>
2014-11-02 19:17:29 +0000

    Running art/imageCompression.py

--- art/imageCompression.py
+++ art/imageCompression.py
@@ -1,8 +1,14 @@
 from tealight.art import (color, box)
 
-def drawGrid(x,y,n,s):
+def drawGrid(x,y,n,s,data):
+  counter = 0
   for i in xrange(0,n):
     for j in xrange(0,n):
+      if data[counter] = 0:
+        color("Black")
+      else:
+        color("white")
       box(x + i*1.5*s,y + j*1.5*s,s,s)
+      counter++
       
 drawGrid(50,50,16,10)
