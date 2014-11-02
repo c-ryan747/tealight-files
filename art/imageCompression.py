9df91fa5aa30c14725067c5aa8595ade64b49d32
c-ryan747 <c.ryan747@gmail.com>
2014-11-02 19:20:55 +0000

    Running art/imageCompression.py

--- art/imageCompression.py
+++ art/imageCompression.py
@@ -12,7 +12,7 @@ def drawGrid(x,y,n,s,data):
   counter = 0
   for i in xrange(0,n):
     for j in xrange(0,n):
-      if data[counter] = 0:
+      if data[counter] == 0:
         color("Black")
       else:
         color("white")
