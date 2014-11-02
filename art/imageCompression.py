632060e37d675e8d5fd54caba818742d6999bca3
c-ryan747 <c.ryan747@gmail.com>
2014-11-02 19:15:19 +0000

    Running art/imageCompression.py

--- art/imageCompression.py
+++ art/imageCompression.py
@@ -3,6 +3,6 @@ from tealight.art import (color, box)
 def drawGrid(x,y,n,s):
   for i in xrange(0,n):
     for j in xrange(0,n):
-      box(i*1.5*s,j*1.5*s,s,s)
+      box(x+ i*1.5*s,j*1.5*s,s,s)
       
 drawGrid(100,100,16,20)
