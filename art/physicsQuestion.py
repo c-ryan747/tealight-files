0840d7b7e568576fae1b33cb6cc74383dc712664
c-ryan747 <c.ryan747@gmail.com>
2015-03-13 13:04:17 +0000

    Running art/physicsQuestion.py

--- art/physicsQuestion.py
+++ art/physicsQuestion.py
@@ -1,3 +1,40 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 import math
 
+x = 300
+y = 200
+vx = 0
+vy = 0
+ax = 0
+ay = 0
+
+def handle_frame(): 
+  global x,y,vx,vy,ax,ay
+  
+  color("white")
+  
+  spot(x,y,8)
+  
+    
+  vx = vx + ax
+  vy = vy + ay
+  
+  
+  #gravity
+  vy = vy + gravity
+  apply_gravity()
+  
+  #friction
+  vx = apply_friction(vx)
+  vy = apply_friction(vy)
+  
+
+  
+  
+  x = x + vx
+  y = y + vy
+  
+  color("blue")
+  
+  spot(x,y,8)
+  
