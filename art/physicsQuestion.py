4476aa7069e76e94859c5245c7afb099587d0fc1
c-ryan747 <c.ryan747@gmail.com>
2015-03-13 16:38:00 +0000

    Running art/physicsQuestion.py

--- art/physicsQuestion.py
+++ art/physicsQuestion.py
@@ -18,8 +18,8 @@ def handle_frame():
   
   angle = math.atan2(-vy,vx) + (math.pi / 2)
   
-  fx =  11 * math.cos(angle)
-  fy = -11 * math.sin(angle)
+  fx =  -1 * math.cos(angle)
+  fy = 1 * math.sin(angle)
   
   # math.sqrt(vx**2 + vy**2)
   vx = vx + fx
