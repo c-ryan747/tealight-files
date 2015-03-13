decda779c1dc175c3bf7907d5b2137367abd217d
c-ryan747 <c.ryan747@gmail.com>
2015-03-13 17:15:33 +0000

    Running art/physicsQuestion.py

--- art/physicsQuestion.py
+++ art/physicsQuestion.py
@@ -1,10 +1,10 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 import math
 
-x = [100,0,0,0,0]
-y = [100,200,300,400,500]
-vx = [5,5,5,5,5]
-vy = [0,0,0,0,0]
+x = [100,0,0,0,0,0]
+y = [100,200,300,400,500,600]
+vx = [5,5,5,5,5,5]
+vy = [0,0,0,0,0,0]
 constant = [0,1,5,7.5,10]
 
 def handle_frame(): 
