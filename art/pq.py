6e44a219701f1cd6905fc4e887039a93d01494c8
c-ryan747 <c.ryan747@gmail.com>
2015-03-13 18:11:19 +0000

    Running art/pq.py

--- art/pq.py
+++ art/pq.py
@@ -1,1 +1,33 @@
-print "A new file!"
+from tealight.art import (color, line, spot, circle, box, image, text, background)
+import math
+
+x =        [100,100,100,100,100,100,100]
+y =        [100,200,300,400,500,600,700]
+vx =       [5,5,5,5,5,5,5]
+vy =       [0,0,0,0,0,0,0]
+constant = [0,0.625,1.25,2.5,5,7.5,10]
+
+def handle_frame(): 
+  global x,y,vx,vy,constant
+
+  for i in range(0,len(x)):
+      color("white")
+      spot(x[i],y[i],2)
+      
+      
+      angle = math.atan2(-vy[i],vx[i]) + (math.pi / 2)
+  
+      fx =  5 * math.cos(angle)
+      fy = -5 * math.sin(angle)
+  
+  
+      vxa = vx[i] + fx
+      vya = vy[i] + fy
+  
+  
+      x[i] = x[i] + vx[i]
+      y[i] = y[i] + vy[i]
+  
+  
+      color("black")
+      spot(x[i],y[i],2)
